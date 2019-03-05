da68711d25492ea3bf544157c4f075dcf290fca8
Elizabeth Zevin <ehzevin@gmail.com>
2019-03-05 16:43:35 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -5,7 +5,7 @@
 
 def display_board(board)
 
-  puts " #{board[1]} | #{board[1]} | #{board[1]} "
+  puts " #{board[1]} | #{board[2]} | #{board[1]} "
   puts "-----------"
   puts " #{board[1]} | #{board[1]} | #{board[1]} "
   puts "-----------"
